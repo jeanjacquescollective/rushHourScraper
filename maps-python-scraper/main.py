58c53b59099afd7df3d234dfd824759baa0a318c
jannes lambrecht <janneslambrecht@hotmail.com>
2023-04-12 22:32:31 +0200

    updated and fixes

--- maps-python-scraper/main.py
+++ maps-python-scraper/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from scrape import getLinks, scrapeDataFromLinks
+from scrape import main
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 app = FastAPI()
+url = 'https://www.google.com/maps/'
+
 
 
 origins = ["*"]
@@ -14,13 +20,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('--blink-settings=imagesEnabled=false')
+driver = webdriver.Chrome(options=options)
+driver.get(url)
+try:
+		cookiesDecliner = WebDriverWait(driver, timeout=1).until(lambda b: b.find_element(By.XPATH,"//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 Nc7WLe']"))
+		cookiesDecliner.click()
+except:
+		pass
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/rushhours/{query}")
-async def getRushhours(query: str):
-    links = getLinks(query)
-    data = scrapeDataFromLinks(links)
+@app.get("/rushhours/{query}/{amount}")
+async def getRushhours(query: str, amount: int):
+    print(query, amount)
+    data = main(driver, query, amount)
     return {'data': data}
--- maps-python-scraper/scrape.py
+++ maps-python-scraper/scrape.py
@@ -7,23 +7,17 @@ from urllib.parse import parse_qs, urlparse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
+# from main import driver
 import csv
 import json
-
-options = webdriver.ChromeOptions()
-
-options.add_argument('headless')
-
-driver = webdriver.Chrome(options=options)
-
-totalData = {}
 
 def writeJson(data):
     with open('json/scrapedPages'+ '' + '.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-def writeCSV(driver,data):   
+
+def writeCSV(title,data):   
     data['hours'].insert(0, 'days')
-    with open('scraps/'+driver.title + '.csv', 'w', encoding='utf-8') as csvfile:
+    with open('scraps/'+title + '.csv', 'w', encoding='utf-8') as csvfile:
             fieldnames = list(data['hours'])
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
@@ -34,10 +28,11 @@ def writeCSV(driver,data):
                     try: 
                         row.append((hour, dict(data[day])[''+hour]))
                     except KeyError:
+                        print(KeyError)
                         row.append((hour, ''))
                 writer.writerow(dict(row))
 
-
+# Function to get day based on number
 def getDay(num):
     switch={
         '1':'Monday',
@@ -56,22 +51,24 @@ def getDay(num):
         '*0':'Sunday'
       }
     return switch.get(num,"Invalid input")
-                
+
+# sort hours 
 def sortHours(arrayToSort):
     arrayToSort = [datetime.strptime(date,'%H:%M') for date in arrayToSort]
     arrayToSort.sort()
     sortedArray =  [date.strftime('%H:%M') for date in arrayToSort]
     return sortedArray
 
-
-def formatHour(hour):
+# convert string from scrape to datetime
+def convertHourToDateTime(hour):
     hour = hour.strip()
     hour = hour.replace('\u202f', ':00 ')
     date = datetime.strptime(hour,'%I:%M %p')
     hour = date.strftime('%H:%M')
     return hour
 
-def getMetaData(driver, url):
+# get meta data from url
+def getMetaDataFromUrl(driver, url):
     latitude = re.search("!3d([0-9a-zA-Z.]+)!?", url).group(1)
     longitude = re.search("!4d([0-9a-zA-Z.]+)!?", url).group(1)
     title = driver.title.split(' - ')[0]
@@ -84,7 +81,8 @@ def getMetaData(driver, url):
         'url': url	
     }
     return metaData
-def scrapeWebsite(url):
+
+def scrapeWebsite(driver, url):
     # instanciate data array
     scrapedData = {
         'peakHours':{
@@ -101,22 +99,17 @@ def scrapeWebsite(url):
         }
     }
     driver.get(url)
-    # bypass cookies
-    # try:
-    #     cookiesDecliner = WebDriverWait(driver, timeout=2).until(lambda b: b.find_element(By.XPATH,"//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 Nc7WLe']"))
-    #     cookiesDecliner.click()
-    # except:
-    #     pass
     print(driver.title)
-    scrapedData['metaData'].update(getMetaData(driver,url))
-
-    el = WebDriverWait(driver, timeout=3).until(lambda b: b.find_element(By.CLASS_NAME,"dpoVLd"))
-    elements = driver.find_elements(By.CLASS_NAME,"dpoVLd")
-    
-    for element in elements:
+		# wait for elements of rush hours to load
+		# these are the bars of the rush hour table
+    WebDriverWait(driver, timeout=1).until(lambda b: b.find_element(By.CLASS_NAME,"dpoVLd"))
+    chartBars = driver.find_elements(By.CLASS_NAME,"dpoVLd")
+    # rewrite to start with aprent element
+    for chartBar in chartBars:
         try:
-            day = getDay(element.find_element(By.XPATH,"./../..").get_attribute("jsinstance"))
-            text    = element.get_attribute('aria-label')
+            # get parent element of the beam
+            day = getDay(chartBar.find_element(By.XPATH,"./../..").get_attribute("jsinstance"))
+            text    = chartBar.get_attribute('aria-label')
             percentage = text.split('%')[0]
             # print(text)
             if 'at' in text.split('%')[1][-6:-1]:
@@ -125,7 +118,7 @@ def scrapeWebsite(url):
                 hour = datetime.now().strftime("%H:00")
                 percentage = text.split('%')[1][-2:]
             else:
-                hour = formatHour(text.split('%')[1][-6:-1])
+                hour = convertHourToDateTime(text.split('%')[1][-6:-1])
             if hour not in scrapedData['peakHours']['hours']:
                 scrapedData['peakHours']['hours'].append(hour) 
             scrapedData['peakHours'][''+day].append((hour,percentage))
@@ -133,47 +126,47 @@ def scrapeWebsite(url):
             print (e)
             continue
     scrapedData['peakHours']['hours'] = sortHours(scrapedData['peakHours']['hours'])
-    # writeCSV(driver, scrapedData['peakHours'])
-    # totalData[''+driver.title] = scrapedData
-    return driver.title.split(' - ')[0], scrapedData
+    scrapedData['metaData'].update(getMetaDataFromUrl(driver,url))
+    scrapedData['metaData']['scrapedAt'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    scrapedData['metaData'].update({
+				'scrapedAt': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'address': driver.find_element(By.CLASS_NAME,"Io6YTe").text,
+		})
+    return scrapedData['metaData']['title'], scrapedData
 
-sites = []
-def getLinks(query, scrolls = 1):
+
+def getLinks(driver, query, amount = 1):
+    sites = []
     url = 'https://www.google.com/maps/search/' + query + '/'
     driver.get(url)
-    try:
-        cookiesDecliner = WebDriverWait(driver, timeout=2).until(lambda b: b.find_element(By.XPATH,"//button[@class='VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc LQeN7 Nc7WLe']"))
-        cookiesDecliner.click()
-    except:
-        pass
-    # resultPanel = WebDriverWait(driver, timeout=2).until(lambda b: b.find_element(By.XPATH,"//div[@class='m6QErb DxyBCb kA9KIf dS8AEf ecceSd']"))
-    for i in range(scrolls):
+    while len(driver.find_elements(By.CLASS_NAME,"hfpxzc")) < amount:
         try:
-            element = WebDriverWait(driver, timeout=3).until(lambda b: b.find_element(By.XPATH,"(//a[@class='hfpxzc'])[last()]"))
+            element = WebDriverWait(driver, timeout=2).until(lambda b: b.find_element(By.XPATH,"(//a[@class='hfpxzc'])[last()]"))
             driver.execute_script("arguments[0].scrollIntoView(true);", element)
-            time.sleep(1)
-        except:
+        except Exception as e:
+            print(e)
             pass
-    links = driver.find_elements(By.XPATH,"//a[@class='hfpxzc']")
+    links = driver.find_elements(By.CLASS_NAME,"hfpxzc")
     for link in links:    
         sites.append(link.get_attribute('href'))
+    print(len(sites))
     return sites
         
-# getLinks('restaurant koblenz')
-
-# sites = ['https://www.google.be/maps/place/Catberry/@51.0410502,3.7226083,15z/data=!4m10!1m2!2m1!1scatberry+gent!3m6!1s0x47c371395c187645:0x69ed7cc1afaf942a!8m2!3d51.0487767!4d3.7341216!15sCg1jYXRiZXJyeSBnZW50Wg8iDWNhdGJlcnJ5IGdlbnSSAQtjb2ZmZWVfc2hvcOABAA!16s%2Fg%2F11tjld_963', 'https://maps.app.goo.gl/cGkpKnhaLvrafbBLA', 'https://maps.app.goo.gl/gfPtMmFhi1QoVprFA']
-# sites = ['https://www.google.be/maps/place/Catberry/@51.0410502,3.7226083,15z/data=!4m10!1m2!2m1!1scatberry+gent!3m6!1s0x47c371395c187645:0x69ed7cc1afaf942a!8m2!3d51.0487767!4d3.7341216!15sCg1jYXRiZXJyeSBnZW50Wg8iDWNhdGJlcnJ5IGdlbnSSAQtjb2ZmZWVfc2hvcOABAA!16s%2Fg%2F11tjld_963', ]
-
-
-def scrapeDataFromLinks(links, amount = 5):
-		for site in sites[:amount]:
+def scrapeDataFromLinks(driver, links, amount = 1):
+		totalData = {}
+		for link in links[:amount]:
 			try:
-					title, data = scrapeWebsite(site)
+					title, data = scrapeWebsite(driver, link)
 					totalData[''+title] = data
-					print('Done with' + title)
+					print('Done with ' + title)
 			except Exception as e:
 					print(e)
 					pass
-		driver.quit() 
+		# driver.close() 
 		return totalData
-# writeJson(totalData)
+
+
+def main(driver, query, amount = 1):
+	links = getLinks(driver, query, amount)
+	data = scrapeDataFromLinks(driver, links, amount)
+	return data
